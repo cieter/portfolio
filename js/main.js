f3b7f62ac7276dd12e9d483ee23cdc64372f3969
Cejay <cjvanwel@hotmail.nl>
2020-10-07 17:32:43 +0200

    More responsiveness and added neet projects section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 // custom cursor
-var parser = new DOMParser();
-var domString = '<div id="cursor"></div>';
-var html = parser.parseFromString(domString, 'text/html');
+let parser = new DOMParser();
+let domString = '<div id="cursor"></div>';
+let html = parser.parseFromString(domString, 'text/html');
 document.body.append(html.body.firstChild);
-var cursor = document.getElementById('cursor');
+let cursor = document.getElementById('cursor');
 
-var body = document.getElementById('body');
-var a = document.querySelectorAll('a');
+let body = document.getElementById('body');
+let a = document.querySelectorAll('a');
+let projectTitle = document.getElementById('projectTitle');
+let projectText = document.getElementById('projectText');
+let projectCard = document.querySelectorAll('.projectCards');
 
 // set loeding screen time
 setTimeout(function(){
@@ -20,6 +23,42 @@ setTimeout(function(){
       });
 }, 3000);  
 
+// for(var i = 0; i < projectCard.length; i++){
+//   projectCard[i].addEventListener('mouseover', function(){
+//     switch(i){
+//       case 0:
+//         // projectTitle.innerHTML = 'Project X';
+//         alert('case is 0');
+//         break;
+//       case 1:
+//         // projectTitle.innerHTML = 'Proftaak';
+//         alert('case is 1');
+//     };
+
+//   });
+// };
+
+projectCard[0].addEventListener('mouseover', function(){
+  projectTitle.innerHTML = 'ProjectX';
+  projectText.innerHTML = 'In dit project moet ik een zelfortret maken. Nee niet zo een van je gezicht. Een zelf protret van je passie en interesses. Daarom ga ik een AR PWA maken, dit is een webapplicatie die digitale elementen over de "echte wereld heen kunnen plakken. Dit gebeurt door middel van augmented reality (AR). Klik op de foto en zie er meer over!!!"';
+});
+
+projectCard[1].addEventListener('mouseover', function(){
+  projectTitle.innerHTML = 'Proftaak';
+  projectText.innerHTML = 'Tijdens dit project hebben we als probleemstelling dat de McFlurry te weinig verkocht wordt. Hiervoor moeten wij ideeën gaan verzinnen waarbij de verkoop meer gestimuleerd wordt. Hiervoor moeten we onderzoek gaan doen naar de doelgroep en de rede achter dat de McFlurry niet zo veel gekocht wordt door mensen. Uit deze onderzoeken komen dan resultaten waar wij, de proftaak leden, dan weer conclusies uit trekken. Klik op de foto en zie wat ik aan deze proftaak heb gedaan!';
+});
+
+function mouseOut(){
+  projectTitle.innerHTML = '';
+  projectText.innerHTML = 'In deze sectie zie je mijn projecten. Als je rechts over 1 van de hovert zie je een korte samenvatting. Als je dan op Meer info klikt ga je naar het project verslag';
+};
+
+//test for touch events support and if not supported, attach .no-touch class to the HTML tag.
+ 
+if (!("ontouchstart" in document.documentElement)) {
+  document.documentElement.className += " no-touch";
+}
+
 // scroll to top on reload
 window.onbeforeunload = function () {
   if(window.scrollTo) window.scrollTo(0,0);
@@ -28,8 +67,8 @@ window.onbeforeunload = function () {
 
 //scrollbar
 window.onscroll = function() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
   document.getElementById("scrollBar").style.width = scrolled + "%";
 }
